9991f3b5e2b2c5e674e43b5a725ee086ba415ab9
Othmane_Rhazlani <oghazlani0@gmail.com>
2021-12-11 16:41:10 +0100

    some update

--- resources/views/template/layouts/header.blade.php
+++ resources/views/template/layouts/header.blade.php
@@ -63,8 +63,64 @@
                     <!--end::Menu wrapper-->
                     <!--begin::Toolbar-->
                     <div class="flex-equal text-end ms-1">
-                        <a href="{{ route('login') }}" class="btn btn-secondary">Sign In</a>
-                        <a href="{{ route('register') }}" class="btn btn-success">Sign Up</a>
+
+                    @guest
+                        @if (Route::has('login'))
+                            <a href="{{ route('login') }}" class="btn btn-secondary">Sign In</a>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="btn btn-success">Sign Up</a>
+                        @endif
+                    @else
+                       <!--begin::Menu-->
+                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
+                                <!--begin::Menu item-->
+                                <div class="menu-item px-3">
+                                    <div class="menu-content d-flex align-items-center px-3">
+                                        <!--begin::Avatar-->
+                                        <div class="symbol symbol-50px me-5">
+                                            <img alt="Logo" src="{{ asset('assets/media/avatars/150-26.jpg')}}" />
+                                        </div>
+                                        <!--end::Avatar-->
+                                        <!--begin::Username-->
+                                        <div class="d-flex flex-column">
+                                            <div class="fw-bolder d-flex align-items-center fs-5">{{ Auth::user()->name }}
+                                            <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2">Pro</span></div>
+                                            <a href="#" class="fw-bold text-muted text-hover-primary fs-7">{{ Auth::user()->email }}</a>
+                                        </div>
+                                        <!--end::Username-->
+                                    </div>
+                                </div>
+                                <!--end::Menu item-->
+                                <!--begin::Menu separator-->
+                                <div class="separator my-2"></div>
+                                <!--end::Menu separator-->
+                                
+                                <!--begin::Menu separator-->
+                                <div class="separator my-2"></div>
+                                <!--end::Menu separator-->
+                                <!--begin::Menu item-->
+                        
+                            
+                                <!--begin::Menu item-->
+                                <div class="menu-item px-5">
+                                    <a href="" class="menu-link px-5" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">Sign Out</a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                </div>
+                    
+                                <!--end::Menu item-->
+                                <!--begin::Menu separator-->
+                                <div class="separator my-2"></div>
+                                <!--end::Menu separator-->
+                            </div>
+                        <!--end::Menu-->
+                    @endguest
                     </div>
                     <!--end::Toolbar-->
                 </div>
